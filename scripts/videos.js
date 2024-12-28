e46634dd8d2d3643f1e7acdc628e33d06a1245e3
OBU <obumaradona@gmail.com>
2024-12-28 09:38:45 +0000

    responsiveness

---
+++ scripts/videos.js
@@ -0,0 +1,88 @@
+export const videos = [
+  {
+    // marquess
+    thumbnail: "images/thumbnails/marques-brownlee.webp",
+    videoLength: "14:20",
+    channelPhoto: "images/profiles/marques-brownlee.jpg",
+    channelName: "Marques Brownlee",
+    channelStats: "200M",
+
+    videoTitle: "Talking Tech and AI with Google CEO Sundar Pichai!",
+    videoStats: {
+      views: "3.4M",
+      datePosted: "6 months"
+    }
+  },
+
+  {
+    thumbnail: "images/thumbnails/fc-pro-open.webp",
+    videoLength: "3:58:56",
+    channelPhoto: "images/profiles/ea-sports-fc-pro.jpg",
+    channelName: "EA SPORTS FC Pro",
+    channelStats: "828K",
+
+    videoTitle: "FC Pro | Open 25 Match Week 1 - Group A",
+    videoStats: {
+      views: "518K",
+      datePosted: "1 month"
+    }
+  },
+
+  {
+    thumbnail: "images/thumbnails/god-of-war.webp",
+    videoLength: "9:30",
+    channelPhoto: "images/profiles/wes-nemo.jpg",
+    channelName: "WesNemo",
+    channelStats: "164K",
+
+    videoTitle: "God of War Just Got BIG NEWS...",
+    videoStats: {
+      views: "140K",
+      datePosted: "1 month"
+    }
+  },
+
+  {
+    thumbnail: "images/thumbnails/mrwhosetheboss-video.webp",
+    videoLength: "27:53",
+    channelPhoto: "images/profiles/mrwhosetheboss.jpg",
+    channelName: "Mrwhosetheboss",
+    channelStats: "20.3M",
+
+    videoTitle: "I tested the Craziest Xiaomi Gadgets!",
+    videoStats: {
+      views: "8.7M",
+      datePosted: "1 month"
+    }
+  },
+
+  {
+    thumbnail: "images/thumbnails/javascript-course.webp",
+    videoLength: "22:15:57",
+    channelPhoto: "images/profiles/super-simple-dev.jpg",
+    channelName: "SuperSimpleDev",
+    channelStats: "567K",
+
+    videoTitle: "JavaScript Tutorial Full Course - Beginner to Pro (2024)",
+    videoStats: {
+      views: "3.1M",
+      datePosted: "7 months"
+    }
+  },
+
+  {
+    thumbnail: "images/thumbnails/guardians-of-the-galaxy.avif",
+    videoLength: "15:48",
+    channelPhoto: "images/profiles/the-canadian-lad.jpg",
+    channelName: "The Canadian Lad",
+    channelStats: "1.79M",
+
+    videoTitle: "I Watched Guardians of the Galaxy Vol. 3 in 0.25x Speed and Here's What I Found",
+    videoStats: {
+      views: "590K",
+      datePosted: "1 year"
+    }
+  },
+
+  
+]
---
+++ scripts/youtube.js
@@ -0,0 +1,82 @@
+import {videos} from './videos.js'
+
+let videosHTML = ''
+
+videos.forEach((video) => {
+  videosHTML += `
+    <div class="video-preview">
+
+      <!-- thumbnail preview -->
+      <div class="thumbnail-div">
+        <img class="thumbnail" src="${video.thumbnail}" alt="${video.channelName}">
+
+        <!-- length of video -->
+        <div class="video-length">
+          ${video.videoLength}
+        </div>
+      </div>
+
+      <!-- Video title, channel photo
+      and other details of video -->
+      <div class="video-info-div">
+        <div class="channel-photo-div">
+          <img class="profile-picture" src="${video.channelPhoto}" alt="">
+
+          <!-- tooltip upon hover -->
+          <div class="channel-tooltip">
+            <div class="channel-profile">
+              <img src="${video.channelPhoto}" alt="">
+            </div>
+
+            <div class="channel-info">
+              <div class="channel-name">
+                ${video.channelName}
+              </div>
+              <div class="channel-stats">
+                ${video.channelStats} Subscribers
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="video-info">
+
+          <!-- video title -->
+          <p class="video-title">
+            ${video.videoTitle}
+          </p>
+          
+          <!-- video author -->
+          <p class="video-author">
+            ${video.channelName}
+          </p>
+
+          <!--  -->
+          <p class="video-stats">
+            ${video.videoStats.views} views &#183; ${video.videoStats.datePosted} ago
+          </p>
+        </div>
+      </div>
+    </div>
+  
+  `
+})
+
+document.querySelector('.js-video-previews-grid')
+  .innerHTML = videosHTML
+
+console.log(videos.length)
+
+const sidebar = document.querySelector('.js-sidebar')
+const hamburgerMenu = document.querySelector('.js-hamburger-menu')
+
+hamburgerMenu.addEventListener('click', () => {
+
+  if (sidebar.style.left === '-100%') {
+    sidebar.style.left = '0'
+  } else {
+    sidebar.style.left = '-100%'
+  }
+
+  
+})
